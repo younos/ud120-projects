70c6b7dcfcc65c85aa35e2c82ec9c86e47d0dcd1
Younos Cherkaoui <younos@protonmail.ch>
2017-11-15 11:09:15 +0100

    Mini-project lecture 14

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -26,7 +26,17 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+### use the train_test_split validation available in sklearn.cross_validation
+### hold out 30% of the data for testing and set the random_state parameter to 42
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
-### it's all yours from here forward!  
-
-
+### create a decision tree classifier (just use the default parameters)
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier()
+### train it on the data
+tree.fit(features_train, labels_train)
+### print out the accuracy
+pred = tree.predict(features_test)
+from sklearn.metrics import accuracy_score
+print 'The accuracy is', accuracy_score(labels_test, pred)
